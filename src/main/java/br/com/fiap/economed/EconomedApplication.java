a36b1ad1fccda59667dc2e836b2a300698f3d0fd
Asteriuz <gutocebola@gmail.com>
2024-04-02 04:25:39 -0300

    Started

---
+++ src/main/java/br/com/fiap/economed/EconomedApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.economed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomedApplication.class, args);
+	}
+
+}
